35cd38d1a10f6f51a7823bc66eba557de271285a
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-17 13:57:32 +0900

    練習1 移動量辞書

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+DELTA = { #移動量辞書
+    pg.K_UP: (0,-5),
+    pg.K_DOWN: (0,5),
+    pg.K_LEFT: (-5,0),
+    pg.K_RIGHT: (5,0),
+    }
 
 
 def main():
@@ -24,14 +30,18 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
