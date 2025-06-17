f691da561aee3e5a977e6f97cc7d5a278b3bebac
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-17 14:36:32 +0900

    練習3 画面内外チェック

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = { #移動量辞書
     }
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRect、または爆弾Rect
+    戻り値：x方向、y方向の画面内判定結果
+    画面内ならTrue、画面外ならFalse
+    """
+    yoko, tate = True, True #初期値：画面内
+    if rct.left < 0 or WIDTH < rct.right: #x方向の画面外判定
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:  #y方向の画面外判定
+        tate = False
+    return yoko, tate #x方向、y方向の画面内判定結果を返す
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -51,8 +65,15 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #移動を打ち消す
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(vx, vy) #爆弾の移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
