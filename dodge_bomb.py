97b36d583d3f7b5562341b25bd50e7eb6a417773
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-17 14:16:26 +0900

    練習2 爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20)) #空のSurface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) #赤い円を描画
+    bb_img.set_colorkey((0, 0, 0)) #黒色を透明色に設定
+    bb_rct = bb_img.get_rect() #爆弾Rectを取得
+    bb_rct.centerx = random.randint(0, WIDTH) #爆弾のx座標を設定
+    bb_rct.centery = random.randint(0, HEIGHT) #爆弾のy座標を設定
+    vx, vy = +5, +5 #爆弾の移動速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +52,8 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
